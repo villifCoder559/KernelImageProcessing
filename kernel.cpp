ecc276d419f0f00ce1a9f938851c8867faddb26c
VillifCoder559 <francesco.villi@gmail.com>
2023-01-11 17:20:47 +0100

    minor fix

--- kernel.cpp
+++ kernel.cpp
@@ -7,35 +7,29 @@ Kernel::Kernel(type_kernel type) {
   switch (type) {
   case edge_detection:
     size = 3;
-    size = 3;
     kernel = {new float[size * size]{-1, -1, -1, -1, 8, -1, -1, -1, -1}};
     break;
   case sharpen:
-    size = 3;
     size = 3;
     kernel = {new float[size * size]{0, -1, 0, -1, 5, -1, 0, -1, 0}};
     break;
   case constant_blur:
     size = 3;
-    size = 3;
     kernel = {new float[size * size]{1.0f / 9.0f, 1.0f / 9.0f, 1.0f / 9.0f, 1.0f / 9.0f, 1.0f / 9.0f, 1.0f / 9.0f, 1.0f / 9.0f, 1.0f / 9.0f, 1.0f / 9.0f}};
     break;
   case gaussian_blur_3x3:
-    size = 3;
     size = 3;
     kernel = {new float[size * size]{1.0f / 16.0f, 2.0f / 16.0f, 1.0f / 16.0f, 2.0f / 16.0f, 4.0f / 16.0f, 2.0f / 16.0f, 1.0f / 16.0f, 2.0f / 16.0f, 1.0f / 16.0f}};
     break;
   case gaussian_blur_5x5:
     size = 5;
-    size = 5;
     kernel = {new float[size * size]{
         1.0f / 256.0f,  4.0f / 256.0f, 6.0f / 256.0f,  4.0f / 256.0f,  1.0f / 256.0f,  4.0f / 256.0f, 16.0f / 256.0f, 24.0f / 256.0f, 16.0f / 256.0f,
         4.0f / 256.0f,  6.0f / 256.0f, 24.0f / 256.0f, 36.0f / 256.0f, 24.0f / 256.0f, 6.0f / 256.0f, 4.0f / 256.0f,  16.0f / 256.0f, 24.0f / 256.0f,
-        16.0f / 256.0f, 4.0f / 256.0f, 1.0f / 256.0f,  4.0f / 256.0f,  6.0f / 256.0f,  4.0f / 256.0f, 1.0f / 256.0f,
+        16.0f / 256.0f, 4.0f / 256.0f, 1.0f / 256.0f,  4.0f / 256.0f,  6.0f / 256.0f,  4.0f / 256.0f, 1.0f / 256.0f
     }};
     break;
   case gaussian_blur_7x7:
-    size = 7;
     size = 7;
     kernel = {new float[size * size]{0.0f,
                                      0.0f,
