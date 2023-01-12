f91461f9ce253eb1b80db59dcc1277b883630567
VillifCoder559 <francesco.villi@gmail.com>
2023-01-12 20:15:17 +0100

    add normalization factor

--- kernel.cpp
+++ kernel.cpp
@@ -7,83 +7,39 @@ Kernel::Kernel(type_kernel type) {
   switch (type) {
   case edge_detection:
     size = 3;
+    normalization_factor = 1.0f;
     kernel = {new float[size * size]{-1, -1, -1, -1, 8, -1, -1, -1, -1}};
     break;
   case sharpen:
     size = 3;
+    normalization_factor = 1.0f;
     kernel = {new float[size * size]{0, -1, 0, -1, 5, -1, 0, -1, 0}};
     break;
   case constant_blur:
     size = 3;
-    kernel = {new float[size * size]{1.0f / 9.0f, 1.0f / 9.0f, 1.0f / 9.0f, 1.0f / 9.0f, 1.0f / 9.0f, 1.0f / 9.0f, 1.0f / 9.0f, 1.0f / 9.0f, 1.0f / 9.0f}};
+    normalization_factor = 1.0f / 9.0f;
+    kernel = {new float[size * size]{1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f, 1.0f}};
     break;
   case gaussian_blur_3x3:
     size = 3;
-    kernel = {new float[size * size]{1.0f / 16.0f, 2.0f / 16.0f, 1.0f / 16.0f, 2.0f / 16.0f, 4.0f / 16.0f, 2.0f / 16.0f, 1.0f / 16.0f, 2.0f / 16.0f, 1.0f / 16.0f}};
+    normalization_factor = 1.0f / 16.0f;
+    kernel = {new float[size * size]{1.0f, 2.0f, 1.0f, 2.0f, 4.0f, 2.0f, 1.0f, 2.0f, 1.0f}};
     break;
   case gaussian_blur_5x5:
     size = 5;
-    kernel = {new float[size * size]{
-        1.0f / 256.0f,  4.0f / 256.0f, 6.0f / 256.0f,  4.0f / 256.0f,  1.0f / 256.0f,  4.0f / 256.0f, 16.0f / 256.0f, 24.0f / 256.0f, 16.0f / 256.0f,
-        4.0f / 256.0f,  6.0f / 256.0f, 24.0f / 256.0f, 36.0f / 256.0f, 24.0f / 256.0f, 6.0f / 256.0f, 4.0f / 256.0f,  16.0f / 256.0f, 24.0f / 256.0f,
-        16.0f / 256.0f, 4.0f / 256.0f, 1.0f / 256.0f,  4.0f / 256.0f,  6.0f / 256.0f,  4.0f / 256.0f, 1.0f / 256.0f
-    }};
+    normalization_factor = 1.0f / 256.0f;
+    kernel = {new float[size * size]{1.0f, 4.0f, 6.0f, 4.0f, 1.0f, 4.0f, 16.0f, 24.0f, 16.0f, 4.0f, 6.0f, 24.0f, 36.0f, 24.0f, 6.0f, 4.0f, 16.0f, 24.0f, 16.0f, 4.0f, 1.0f, 4.0f, 6.0f, 4.0f, 1.0f}};
     break;
   case gaussian_blur_7x7:
     size = 7;
-    kernel = {new float[size * size]{0.0f,
-                                     0.0f,
-                                     1.0f / 1003.0f,
-                                     2.0f / 1033.0f,
-                                     1.0f / 1033.0f,
-                                     0.0f,
-                                     0.0f,
-                                     0.0f,
-                                     3.0f / 1003.0f,
-                                     13.0f / 1003.0f,
-                                     22.0f / 1003.0f,
-                                     13.0f / 1003.0f,
-                                     3.0f / 1003.0f,
-                                     0.0f,
-                                     1.0f / 1003.0f,
-                                     13.0f / 1003.0f,
-                                     59.0f / 1003.0f,
-                                     97.0f / 1003.0f,
-                                     59.0f / 1003.0f,
-                                     13.0f / 1003.0f,
-                                     1.0f / 1003.0f,
-                                     2.0f / 1003.0f,
-                                     22.0f / 1003.0f,
-                                     97.0f / 1003.0f,
-                                     159.0f / 1003.0f,
-                                     97.0f / 1003.0f,
-                                     22.0f / 1003.0f,
-                                     2.0f / 1003.0f,
-                                     1.0f / 1003.0f,
-                                     13.0f / 1003.0f,
-                                     59.0f / 1003.0f,
-                                     97.0f / 1003.0f,
-                                     59.0f / 1003.0f,
-                                     13.0f / 1003.0f,
-                                     1.0f / 1003.0f,
-                                     0.0f,
-                                     3.0f / 1003.0f,
-                                     13.0f / 1003.0f,
-                                     22.0f / 1003.0f,
-                                     13.0f / 1003.0f,
-                                     3.0f / 1003.0f,
-                                     0.0f,
-                                     0.0f,
-                                     0.0f,
-                                     1.0f / 1003.0f,
-                                     2.0f / 1003.0f,
-                                     1.0f / 1003.0f,
-                                     0.0f,
-                                     0.0f}};
+    normalization_factor = 1.0f / 1033.f;
+    kernel = {new float[size * size]{0.0f,  0.0f,  1.0f,  2.0f,  1.0f,  0.0f,  0.0f,  0.0f,   3.0f,  13.0f, 22.0f, 13.0f, 3.0f,  0.0f,  1.0f,  13.0f, 59.0f,
+                                     97.0f, 59.0f, 13.0f, 1.0f,  2.0f,  22.0f, 97.0f, 159.0f, 97.0f, 22.0f, 2.0f,  1.0f,  13.0f, 59.0f, 97.0f, 59.0f, 13.0f,
+                                     1.0f,  0.0f,  3.0f,  13.0f, 22.0f, 13.0f, 3.0f,  0.0f,   0.0f,  0.0f,  1.0f,  2.0f,  1.0f,  0.0f,  0.0f}};
     break;
   }
 }
-
+float Kernel::get_normalization_factor() { return normalization_factor; }
 int Kernel::get_size() { return size; }
 float *Kernel::get_kernel() { return kernel; }
 Kernel::~Kernel() { delete[] kernel; }
