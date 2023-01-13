99af9a91e51eac7c6d4d793bf89ee0ebbbe4cc02
VillifCoder559 <francesco.villi@gmail.com>
2023-01-13 22:18:21 +0100

    clean code

--- kernel.h
+++ kernel.h
@@ -1,11 +1,14 @@
 #ifndef KERNEL
 #define KERNEL
+
 enum type_kernel { edge_detection, sharpen, constant_blur, gaussian_blur_3x3, gaussian_blur_5x5, gaussian_blur_7x7 };
+
 class Kernel {
 private:
   int size;
   float *kernel;
   float normalization_factor;
+
 public:
   Kernel(type_kernel type);
   /*
@@ -13,7 +16,7 @@ public:
   */
   int get_size();
   float get_normalization_factor();
-  float* get_kernel();
+  float *get_kernel();
   ~Kernel();
 };
 #endif
